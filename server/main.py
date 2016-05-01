dd9e8b2ba84cfc7f0ad070cb364580347aa97886
Daniel Bramucci <dbramuccicode@gmail.com>
2016-05-01 03:50:54 -0500

    Added text message support.

--- server/main.py
+++ server/main.py
@@ -1,11 +1,20 @@
 from flask import Flask, request
 from person import Person
 from test import make_test_case
+from twilio.rest import TwilioRestClient
+
+account_sid = ""
+auth_token = ""
+client = TwilioRestClient(account_sid, auth_token)
 
 app = Flask(__name__)
 
 GROUP_RADIUS = 100
 group = {}
+
+def send_lost_message(phone_number, lost_person_name):
+    message = client.messages.create(to=0, from_=0,
+                                     body="{} is being left behind!".format(lost_person_name))
 
 def group_center(group: set) -> tuple:
     center = [0, 0]
@@ -29,26 +38,33 @@ def find_lost_people(list_of_group_members: list) -> set:
             lost_people.add(person_lost)
     return lost_people
 
+
 @app.route("/", methods=['GET', 'POST'])
 def get_data():
     if request.method == 'POST':
         try:
-            name, phone_num, lat, long = str(request.data).split(':')
-            lat, long = float(lat), float(long[:-1])
+            post_data = request.args
+            name = request.args['name']
+            phone_num = int(request.args['num'])
+            lat = float(post_data['lat'])
+            long = float(post_data['long'])
             print(name, phone_num, lat, long)
-            group[phone_num] = Person(name[2:], phone_num)
+            group[phone_num] = Person(name, phone_num)
             group[phone_num].coordinates = lat, long
         except (ValueError, RuntimeError):
-            return "Invalid data"
+            return '404'
         main()
-        return "Message received"
+        return '200'
+
 
 def main():
     # group = make_test_case()
-    lost_people = find_lost_people(group)
-    for i in group.values():
-        print(i)
+    lost_people = find_lost_people(group.values())
+    for i in lost_people:
+        if len(group) > 1:
+            print(i)
+            send_lost_message(i.phone_number, i.name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
