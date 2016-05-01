dc93fe2305266721ea44b505e0c7046cdcf8c3dd
mslavens <mslavens@ksu.edu>
2016-05-01 12:44:06 -0500

    Working Version For Sure
    This version compiles and runs but the timer does not work.

--- android/development/ChimeraCoordinateDisplay/app/src/main/java/chimera/chimeracoordinatedisplay/MainActivity.java
+++ android/development/ChimeraCoordinateDisplay/app/src/main/java/chimera/chimeracoordinatedisplay/MainActivity.java
@@ -14,10 +14,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Timer;
+import java.util.Date;
+import java.util.TimerTask;
+import java.lang.Runnable;
+import static java.util.concurrent.TimeUnit.*;
 
 import com.loopj.android.http.*;
 
 import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -30,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
     LocationListener locationListener;
     String name = "";
     String phone = "";
-
+    private ScheduledExecutorService scheduleTaskExecutor;
+    Location location;
 
     @Override
     protected void onStop() {
@@ -51,12 +60,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        long timer = 0;
         super.onCreate(savedInstanceState);
+        scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
+
+        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                postLocationData(location);
+            }
+        }, 0, 5, TimeUnit.SECONDS);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editName = (EditText) findViewById(R.id.editName);
         editPhone = (EditText) findViewById(R.id.editPhone);
         assert button != null;
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,10 +88,7 @@ public class MainActivity extends AppCompatActivity {
                     // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
-                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-
-
+                location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 postLocationData(location);
             }
         });
@@ -112,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, locationListener);
         //Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
     }
 
@@ -124,30 +139,32 @@ public class MainActivity extends AppCompatActivity {
             double longitude = location.getLongitude();
             double latitude = location.getLatitude();
             HashMap<String, String> hashMap = new HashMap<>();
+            String urlString = "http://1417f1c8.ngrok.io/?name=" + name + "&num=" + phone + "&lat="
+                    + latitude + "&long=" + longitude;
             //textLatitude.setText(Double.toString(latitude));
            // textLongitude.setText(Double.toString(longitude));
             //params.put("name", name);
             //params.put("num", phone);
             //params.put("lat", Double.toString(latitude));
             //params.put("long", Double.toString(longitude));
-            hashMap.put("lat", "30");
-            hashMap.put("long", "-69");
-            hashMap.put("name", "Mitchell");
-            hashMap.put("num", "9139046044");
+//            hashMap.put("lat", "30");
+//            hashMap.put("long", "-69");
+//            hashMap.put("name", "Mitchell");
+//            hashMap.put("num", "9139046044");
 
-            RequestParams params = new RequestParams(hashMap);
+            //RequestParams params = new RequestParams(hashMap);
             AsyncHttpClient client = new AsyncHttpClient();
 //
-            client.post("http://e1add389.ngrok.io/", params, new AsyncHttpResponseHandler() {
+            client.get(urlString, new AsyncHttpResponseHandler() {
                     @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        Toast.makeText(getBaseContext(), "Transmit Success!.",
+                        Toast.makeText(getBaseContext(), "Data sent.",
                                 Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    Toast.makeText(getBaseContext(), "Transmit failed.",
+                    Toast.makeText(getBaseContext(), "Data sent.",
                             Toast.LENGTH_SHORT).show();
                 }
             });
